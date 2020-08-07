b019959879fe77077989ccbb9fa94354e9dc909a
Ilia <iliacomarov@gmail.com>
2020-08-08 00:45:34 +0300

    updated provision

--- provision.sh
+++ provision.sh
@@ -2,4 +2,4 @@
 
 mongo --eval "db = db.getSiblingDB(\"admin\"); db.createUser({ user: \"$MONGO_ADMIN\", pwd: \"$MONGO_ADMIN_PASSWORD\", roles: [{ role: \"userAdminAnyDatabase\", db: \"admin\" }, { role: \"readWriteAnyDatabase\", db: \"admin\" }]});";
 mongo --eval "db = db.getSiblingDB(\"$MONGO_DB\"); db.createUser({ user: \"$MONGO_USER\", pwd: \"$MONGO_PASSWORD\", roles: [{ role: \"readWrite\", db: \"$MONGO_DB\" }]});";
-mongo --eval "db.$MONGO_DB.insertOne({'firstname': 'Sarah', 'lastname': 'Smith'});";
+mongo --eval "db.getSiblingDB(\"$MONGO_DB\"); db.uniq.insertOne({'firstname': 'Sarah', 'lastname': 'Smith'});";
